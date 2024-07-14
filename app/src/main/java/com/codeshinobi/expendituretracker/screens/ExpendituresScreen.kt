12aa8df4ab9a2a8f1248eed849f7164d60a84be4
darlingson <dmakuwila@gmail.com>
2024-07-14 11:05:30 +0200

    moved contents of the home screem to the expenditures screen

--- app/src/main/java/com/codeshinobi/expendituretracker/screens/ExpendituresScreen.kt
+++ app/src/main/java/com/codeshinobi/expendituretracker/screens/ExpendituresScreen.kt
@@ -1,19 +1,50 @@
 package com.codeshinobi.expendituretracker.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.codeshinobi.expendituretracker.Screens
 import com.codeshinobi.expendituretracker.ui.theme.ExpenditureTrackerTheme
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Regular
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.regular.Keyboard
+import compose.icons.fontawesomeicons.solid.CameraRetro
+import compose.icons.fontawesomeicons.solid.PhotoVideo
 
+data class ExpendituresGridItemData(val name: String, val icon: ImageVector, val onClick: () -> Unit)
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ExpendituresScreen(navController: NavController) {
     ExpenditureTrackerTheme {
@@ -21,30 +52,115 @@ fun ExpendituresScreen(navController: NavController) {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
+            val items = listOf(
+                ExpendituresGridItemData(
+                    name = "Scan Receipt",
+                    icon = FontAwesomeIcons.Solid.CameraRetro,
+                    onClick = { navController.navigate(Screens.ReceiptScan.route) }
+                ),
+                ExpendituresGridItemData(
+                    name = "Gallery",
+                    icon = FontAwesomeIcons.Solid.PhotoVideo,
+                    onClick = { navController.navigate(Screens.Gallery.route) }
+                ),
+                ExpendituresGridItemData(
+                    name = "Enter manually",
+                    icon = FontAwesomeIcons.Regular.Keyboard,
+                    onClick = { navController.navigate(Screens.ManualEntry.route) }
+                )
+            )
             Column(
-                modifier = Modifier.fillMaxSize().padding(15.dp),
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Expenditure Tracker",
+                    style = MaterialTheme.typography.headlineLarge,
+                    modifier = Modifier.padding(top = 32.dp, bottom = 16.dp)
+                )
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(minSize = 120.dp),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                ) {
+                    items(items.size) { index ->
+                        val item = items[index]
+                        ExpenditureGridItem(name = item.name, icon = item.icon, onClick = item.onClick)
+                    }
+                }
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExpenditureGridItem(
+    modifier: Modifier = Modifier,
+    name: String,
+    icon: ImageVector,
+    onClick: () -> Unit
+) {
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+            .padding(8.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            MaterialTheme.colorScheme.primary.copy(alpha = 0.7f),
+                            MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+                        )
+                    )
+                )
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-//                Box(
-//                    modifier = Modifier.fillMaxWidth()
-//                        .height(200.dp)
-//                        .padding(horizontal = 15.dp, vertical = 10.dp)
-//                        .clip(MaterialTheme.shapes.large)
-//                ) {
-//                    Image(
-//                        painter = painterResource(R.drawable.images),
-//                        contentDescription = "home_screen_bg",
-//                        contentScale = ContentScale.Crop,
-//                        modifier = Modifier.fillMaxSize()
-//                    )
-//                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.secondary, CircleShape),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = name,
+                        modifier = Modifier.fillMaxSize(),
+                        tint = MaterialTheme.colorScheme.onSecondary
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(
-                    "Expenditures Screen",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(vertical = 20.dp)
+                    text = name,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp,
+                        shadow = Shadow(
+                            color = Color.Black.copy(alpha = 0.2f),
+                            offset = Offset(2f, 2f),
+                            blurRadius = 4f
+                        )
+                    ),
+                    color = MaterialTheme.colorScheme.onPrimary
                 )
             }
         }
     }
+}
+@Composable
+fun Placeholder() {
+
 }
