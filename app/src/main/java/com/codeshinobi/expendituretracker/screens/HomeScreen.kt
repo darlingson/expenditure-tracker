83b655050abad508d6b1d9c216f57cd49b55ecf3
darlingson <dmakuwila@gmail.com>
2024-07-15 21:23:23 +0200

    added overview on the homepage

--- app/src/main/java/com/codeshinobi/expendituretracker/screens/HomeScreen.kt
+++ app/src/main/java/com/codeshinobi/expendituretracker/screens/HomeScreen.kt
@@ -4,39 +4,47 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.codeshinobi.expendituretracker.data.ExpensesViewModel
 import com.codeshinobi.expendituretracker.ui.theme.ExpenditureTrackerTheme
 
 
 @Composable
-fun HomeScreen(navController: NavController) {
+fun HomeScreen(navController: NavController, viewModel: ExpensesViewModel = viewModel(factory = ExpensesViewModel.Factory)) {
+    val LatestExpenses by viewModel.getLatest().collectAsState(initial = emptyList())
     ExpenditureTrackerTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
             Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(15.dp),
+                modifier = Modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
             ) {
                 Text(
-                    "Home Screen",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(vertical = 20.dp)
+                    text = "Latest Expenditures",
+                    style = MaterialTheme.typography.headlineLarge,
+                    modifier = Modifier.padding(top = 32.dp, bottom = 16.dp)
                 )
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(LatestExpenses.size) { expense ->
+                        ExpenseItem(expense = LatestExpenses[expense])
+                    }
+                }
             }
         }
     }
 }
-
-
