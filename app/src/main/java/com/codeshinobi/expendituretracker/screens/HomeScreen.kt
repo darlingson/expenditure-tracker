5606aa5c751b9ad5ba7aed3d4cdce7dd19f39b26
darlingson <dmakuwila@gmail.com>
2024-07-20 16:52:02 +0200

    added text to show total amount spent recently

--- app/src/main/java/com/codeshinobi/expendituretracker/screens/HomeScreen.kt
+++ app/src/main/java/com/codeshinobi/expendituretracker/screens/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.codeshinobi.expendituretracker.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -23,7 +24,13 @@ import com.codeshinobi.expendituretracker.ui.theme.ExpenditureTrackerTheme
 
 @Composable
 fun HomeScreen(navController: NavController, viewModel: ExpensesViewModel = viewModel(factory = ExpensesViewModel.Factory)) {
-    val LatestExpenses by viewModel.getLatest().collectAsState(initial = emptyList())
+    val latestExpenses by viewModel.getLatest().collectAsState(initial = emptyList())
+    var totalLatestExpenses by mutableStateOf(0.0)
+
+    // calculate total latest expenses
+    latestExpenses.forEach { expense ->
+        totalLatestExpenses += expense.amount
+    }
     ExpenditureTrackerTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
@@ -31,18 +38,22 @@ fun HomeScreen(navController: NavController, viewModel: ExpensesViewModel = view
         ) {
             Column(
                 modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
+/*                horizontalAlignment = Alignment.CenterHorizontally,*/
             ) {
                 Text(
                     text = "Latest Expenditures",
                     style = MaterialTheme.typography.headlineLarge,
                     modifier = Modifier.padding(top = 32.dp, bottom = 16.dp)
                 )
+                Text(
+                    "amount spent the last ${latestExpenses.size} times : $totalLatestExpenses",
+                    modifier = Modifier.padding(top = 8.dp, bottom = 16.dp, start = 16.dp)
+                )
                 LazyColumn(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    items(LatestExpenses.size) { expense ->
-                        ExpenseItem(expense = LatestExpenses[expense])
+                    items(latestExpenses.size) { expense ->
+                        ExpenseItem(expense = latestExpenses[expense])
                     }
                 }
             }
